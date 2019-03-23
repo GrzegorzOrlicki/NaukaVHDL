959e2efebdc86c9ebaea1243d7589753217f5349
Grzegorz Orlicki <grzorl2907@gmail.com>
2019-03-23 15:05:17 +0100

    poprawa 1 z 4

--- zamiana_z_bin_na_1z4.vhd
+++ zamiana_z_bin_na_1z4.vhd
@@ -1,7 +1,7 @@
 -- zamiana z kodu binarnego na kod 1 z 4 z aktywnym zerem
 with bin select
-    kod1z4 <= b"1110" when "00",
-            b"1101" when "01",
-            b"1011" when "10",
-            b"0111" when "11",
-            "-" when others;
+    kod1z4 <= b"1110" when b"00",
+            b"1101" when b"01",
+            b"1011" when b"10",
+            b"0111" when b"11",
+            "-" when others; -- stan nieustalony na nieprawidłowych wejściach
